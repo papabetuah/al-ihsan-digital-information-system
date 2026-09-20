8ca662b3d51147ce6ee2de095addde499b73e4b7
Dodi Febrizal <dodifebrizal@gmail.com>
2026-09-20 18:30:49 +0700

    Add robust prayer phases and Friday timing controls

--- config.js
+++ config.js
@@ -1,1 +1,21 @@
-window.AL_IHSAN_CONFIG={name:"Masjid Al Ihsan Kapuih",location:"Jalan Kapuih, Kel. Limau Manis, Kec. Pauh, Padang",latitude:-0.9182762793204684,longitude:100.46781691534656,timezone:"Asia/Jakarta",tz:7,spreadsheetId:"1R3mRlgPxlEFvarBZma_laJHoEiVDvLznXAqfAQyUHpE",refreshMinutes:5,preAdhanMinutes:10,prayerModeMinutes:{shubuh:20,dzuhur:20,ashar:20,maghrib:15,isya:20},friday:{prepMinutes:30,khutbahStart:"12:10",end:"13:05"},playlist:[["dashboard",20],["pengumuman",15],["agenda",15],["keuangan",20],["pembangunan",20],["donasi",20],["dokumentasi",15],["dakwah",15]],angles:{fajr:20,isha:18,asrFactor:1,sunriseSunset:.833}};
+window.AL_IHSAN_CONFIG={
+  name:"Masjid Al Ihsan Kapuih",
+  location:"Jalan Kapuih, Kel. Limau Manis, Kec. Pauh, Padang",
+  latitude:-0.9182762793204684,
+  longitude:100.46781691534656,
+  timezone:"Asia/Jakarta",
+  tz:7,
+  spreadsheetId:"1R3mRlgPxlEFvarBZma_laJHoEiVDvLznXAqfAQyUHpE",
+  refreshMinutes:5,
+  fetchTimeoutMs:8000,
+  preAdhanMinutes:10,
+  iqamahMinutes:{shubuh:10,dzuhur:10,ashar:10,maghrib:5,isya:10},
+  prayerDurationMinutes:{shubuh:15,dzuhur:12,ashar:12,maghrib:10,isya:12},
+  friday:{prepMinutes:30,khutbahStart:"12:10",prayerStart:"12:45",end:"13:05"},
+  manualMode:"AUTO",
+  playlist:[
+    ["dashboard",20],["pengumuman",15],["agenda",15],["keuangan",20],
+    ["pembangunan",20],["donasi",20],["dokumentasi",15],["dakwah",15]
+  ],
+  angles:{fajr:20,isha:18,asrFactor:1,sunriseSunset:.833}
+};
