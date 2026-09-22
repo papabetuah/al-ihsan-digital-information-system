8bb616cea59591c6c59d91bc664ac80a08a35481
Dodi Febrizal <dodifebrizal@gmail.com>
2026-09-22 16:22:56 +0700

    Set dashboard release to v9

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.AL_IHSAN_CONFIG = Object.freeze({
   name: "Masjid Al Ihsan Kapuih",
   timezone: "Asia/Jakarta",
   timezoneOffset: 7,
-  release: "tarjih-padang-v8",
+  release: "tarjih-padang-v9",
   stage: Object.freeze({ width: 1672, height: 941 }),
   prayerMethod: Object.freeze({
     id: "majelis-tarjih-muhammadiyah-padang",
