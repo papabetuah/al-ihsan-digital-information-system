82f38f644af7f158f84be48c888f19765de92277
Dodi Febrizal <dodifebrizal@gmail.com>
2026-09-22 08:26:33 +0700

    Upgrade service-worker.js to calibrated Tarjih Padang engine

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="alihsan-tarjih-padang-1";
-const FILES=["./","./index.html","./styles.css","./app.js","./config.js","./assets/master-dashboard-approved-final.png"];
+const CACHE="alihsan-tarjih-padang-v1";
+const FILES=["./","./index.html","./styles.css","./app.js","./config.js","./prayer-times.js","./assets/master-dashboard-approved-final.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
