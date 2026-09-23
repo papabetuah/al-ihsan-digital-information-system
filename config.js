66ec808bdd78eacede9ed8c8078d77035b0ead16
Dodi Febrizal <dodifebrizal@gmail.com>
2026-09-23 18:03:29 +0700

    Refine overlay typography and clock alignment

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.AL_IHSAN_CONFIG = Object.freeze({
   name: "Masjid Al Ihsan Kapuih",
   timezone: "Asia/Jakarta",
   timezoneOffset: 7,
-  release: "underlay-final-20260923-v16",
+  release: "underlay-final-20260923-v17",
   stage: Object.freeze({ width: 1672, height: 941 }),
   prayerMethod: Object.freeze({
     id: "majelis-tarjih-muhammadiyah-padang",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="alihsan-underlay-final-20260923-v16";
+const CACHE="alihsan-underlay-final-20260923-v17";
 const FILES=[
   "./",
   "./index.html",
