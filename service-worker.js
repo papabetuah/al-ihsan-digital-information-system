39724bf1181cc2c34e2a7435aed6e958237ac2b3
Dodi Febrizal <dodifebrizal@gmail.com>
2026-09-23 15:31:21 +0700

    Use verified owner-uploaded underlay on production web

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ const FILES=[
   "./app.js",
   "./config.js",
   "./prayer-times.js",
-  "./assets/dashboard-underlay-active.jpg"
+  "./assets/dashboard-underlay-active.webp"
 ];
 
 self.addEventListener("install",event=>{
